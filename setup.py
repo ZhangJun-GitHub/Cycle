7534d84d3bcd2c71b5febd628bc83bbcd94c6d2c
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2011-09-20 11:35:24 +0100

    Changed version strings, will tag this as 0.3.2

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@
 from distutils.core import setup
 
 setup (name = "cycle",
-       version = "0.3.1",
+       version = "0.3.2",
        description = "Calendar for women",
        author = "Oleg Gints",
-       author_email = "altgo@users.sourceforge.net",
-       url = "http://cycle.sourceforge.net",
+       maintainer = "Matt Molyneaux",
+       maintainer_email = "moggers87+git@moggers87.co.uk",
+       url = "http://moggers.co.uk/cgit/cycle.git/about",
        py_modules = ['cycle'],
+       license = "GPLv2",
       )
 
